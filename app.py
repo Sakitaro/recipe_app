e328337f3127376bc75d684969dc162791830f07
Iwanami Sakitaro <a5819014@aoyama.jp>
2023-05-07 13:38:00 +0900

    image追加

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, f
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from PIL import Image
 
 import os
 from recipe_app import request_recipe, request_instructions
@@ -14,14 +16,32 @@ class User:
         self.create_table()
 
     def create_table(self):
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                       (id INTEGER PRIMARY KEY,
+                        username TEXT UNIQUE NOT NULL,
+                        email TEXT UNIQUE NOT NULL,
+                        password TEXT NOT NULL)''')
+        self.conn.commit()
+
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS recipes
                        (id INTEGER PRIMARY KEY,
                         title TEXT NOT NULL,
                         ingredients TEXT NOT NULL,
                         instructions TEXT NOT NULL,
                         user_id INTEGER,
+                        image_filename TEXT,
                         FOREIGN KEY(user_id) REFERENCES users(id))''')
         self.conn.commit()
+
+        # Check if the image_filename column exists
+        self.cursor.execute("PRAGMA table_info(recipes)")
+        columns = self.cursor.fetchall()
+        column_names = [column[1] for column in columns]
+        
+        # If the image_filename column does not exist, add it
+        if 'image_filename' not in column_names:
+            self.cursor.execute("ALTER TABLE recipes ADD COLUMN image_filename TEXT")
+            self.conn.commit()
 
     def add_user(self, username, email, password):
         self.cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, password))
@@ -32,14 +52,14 @@ class User:
         user = self.cursor.fetchone()
         return user
 
-    def add_recipe(self, user_id, title, ingredients, instructions):
-        self.cursor.execute("INSERT INTO recipes (user_id, title, ingredients, instructions) VALUES (?, ?, ?, ?)", (user_id, title, ingredients, instructions))
+    def add_recipe(self, user_id, title, ingredients, instructions, image_filename):
+        self.cursor.execute("INSERT INTO recipes (user_id, title, ingredients, instructions, image_filename) VALUES (?, ?, ?, ?, ?)", (user_id, title, ingredients, instructions, image_filename))
         self.conn.commit()
 
     def get_all_recipes(self):
-        self.cursor.execute("""SELECT recipes.id, recipes.title, recipes.ingredients, recipes.instructions, users.username
-                             FROM recipes
-                             JOIN users ON recipes.user_id = users.id""")
+        self.cursor.execute("""SELECT recipes.id, recipes.title, recipes.ingredients, recipes.instructions, users.username, recipes.image_filename
+                            FROM recipes
+                            JOIN users ON recipes.user_id = users.id""")
         recipes = self.cursor.fetchall()
         return recipes
 
@@ -59,7 +79,8 @@ def before_request():
 
 @app.teardown_request
 def teardown_request(exception):
-    g.user_db.close()
+    if hasattr(g, 'user_db'):
+        g.user_db.close()
 
 @app.route("/")
 def index():
@@ -115,20 +136,46 @@ def logout():
     flash('Logged out.', 'info')
     return redirect(url_for('index'))
 
+# 画像の保存
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def resize_image(image_path, max_size):
+    image = Image.open(image_path)
+    image.thumbnail(max_size)
+    image.save(image_path)
+
 @app.route('/post_recipe', methods=['GET', 'POST'])
 def post_recipe():
     if request.method == 'POST':
+        # 画像ファイルをチェックし、保存
+        image = request.files.get('image')
+        if not image or not allowed_file(image.filename):
+            flash("Invalid image format. Allowed formats: jpg, jpeg, png, gif", "danger")
+            return redirect(url_for('post_recipe'))
+
+        filename = secure_filename(image.filename)
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image.save(image_path)
+        resize_image(image_path, (800, 800))
+
         title = request.form['title']
         ingredients = request.form['ingredients']
         instructions = request.form['instructions']
 
-        g.user_db.add_recipe(session.get('user_id'), title, ingredients, instructions)
+        g.user_db.add_recipe(session.get('user_id'), title, ingredients, instructions, filename)
         g.user_db.conn.commit()
 
-        return redirect(url_for('index'))
+        return redirect(url_for('post_recipe')) 
 
     recipes = g.user_db.get_all_recipes()
     return render_template('post_recipe.html', recipes=recipes)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
